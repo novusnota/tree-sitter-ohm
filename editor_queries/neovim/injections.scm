6bc9c10c6f7dfc10d917880f9d01bcb7a81c0b0e
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-02-29 18:09:59 +0100

    feat: injections.scm for Neovim

---
+++ editor_queries/neovim/injections.scm
@@ -0,0 +1,7 @@
+; See: https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md#injections
+
+((singleline_comment) @injection.content
+  (#set! injection.language "comment"))
+
+((multiline_comment) @injection.content
+  (#set! injection.language "comment"))
